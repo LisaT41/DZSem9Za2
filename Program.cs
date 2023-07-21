0fc57d74bccb348fefa917b21d3205127bd2e765
l.gor41000@yandex.ru <l.gor41000@yandex.ru>
2023-07-21 20:15:29 +0200

    seconde task

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int m = InputInt("Input M:");
+int n = InputInt("Input N:");
+Console.WriteLine($"Sum of elements from {m} to {n} = {CountNaturalSum(m, n)}");
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+int CountNaturalSum(int m, int n)
+{
+    if (m == n)
+        return n;
+    return n + CountNaturalSum(m, n - 1);
+}
